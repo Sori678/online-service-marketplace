49370be7fa7f002698461296ff95af42fe779879
Sori678 <biv7sorin@gmail.com>
2026-01-12 09:45:32 +0100

    Created Services page

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.views import Service
 # Create your views here.
+from django.shortcuts import render
+from .models import Service
 
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'marketplace/services.html', {'services': services})
