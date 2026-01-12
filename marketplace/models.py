8b105fa6cd185a812279f08aa6af9795aabb91d0
Sori678 <biv7sorin@gmail.com>
2026-01-12 09:06:33 +0100

    Created model Service

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Service(models.Model):
+    CATEGORY_CHOICES = [
+        ('cleaning', 'Cleaning'),
+        ('it', 'IT & Software'),
+        ('math', 'Mathematics Meditations'),
+        ('garden', 'Garden'),
+    ]
+
+    title = models.CharField(max_length=200, unique=True)
+    provider = models.ForeignKey(User, on_delete=models.CASCADE, related_name="services")
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} | {self.provider.username}"
