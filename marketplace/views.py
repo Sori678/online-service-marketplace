0b66c97ddcd30786cc574d29e84c756086fa3065
Sori678 <biv7sorin@gmail.com>
2026-01-12 09:58:22 +0100

    Conected page to project

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.views import Service
-# Create your views here.
-from django.shortcuts import render
 from .models import Service
+# Create your views here.
 
 def service_list(request):
     services = Service.objects.all()
