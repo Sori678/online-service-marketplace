9709024d2e1da6cbc6f76dd0953c2adb61a0c59c
Sori678 <biv7sorin@gmail.com>
2026-01-14 10:09:06 +0100

    adding the ability to view the detailed service

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.service_list, name='service_list'),
     path('create/', views.create_service, name='create_service'),
+    path('service/<int:service_id>/', views.service_detail, name='service_detail'),
+    
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 from django.shortcuts import redirect
 from .forms import ServiceForm
@@ -19,3 +19,7 @@ def create_service(request):
     else:
         form = ServiceForm()
     return render(request, 'marketplace/service_form.html', {'form': form})
+
+def service_detail(request, service_id):
+    service = get_object_or_404(Service, id=service_id)
+    return render(request, 'marketplace/service_detail.html', {'service': service})
